a1e7af5ca5acd527b4c571b1a02a9a60532032bc
gotca dumitru <dumitru.gotca@assist.ro>
2022-02-28 11:58:54 +0200

    add react

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from 'react';
+
+const App = () => {
+  return <div className='template'>My React app with three js template + scss</div>;
+};
+export default App;
--- src/script.js
+++ src/script.js
@@ -1,4 +1,8 @@
 import Experience from './Experience/Experience';
-import './style.css';
+import './style.scss';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 const experience = new Experience(document.querySelector('canvas.webgl'));
+ReactDOM.render(<App />, document.getElementById('root'));
